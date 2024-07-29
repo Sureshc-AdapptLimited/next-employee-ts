aebbc6d9fef0aa4dc036d94e65a8a7c7bd2e47c6
Sureshc-AdapptLimited <suresh.c@adappt.ai>
2024-07-29 11:44:12 +0530

    Navigate the employee page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import EmployeePage from "./components/EmployeePage";
 import EmployeeModels from "./components/employees/EmployeeModels";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
+     <Link href="/employees" className="text-blue-500 hover:text-blue-700">
+        Go to Employee Page
+      </Link>
       {/* <EmployeePage /> */}
-      <EmployeeModels></EmployeeModels>
+      {/* <EmployeeModels></EmployeeModels> */}
     </div>
   );
 }
